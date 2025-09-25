81c908d36070298b99662873091a468882cd5ac0
Jose Fernando Gonzales <josefernando.gonzales@digiph.asia>
2025-09-25 13:35:39 +0800

    Add SQL scripts to initialize vector extension and create documents table

---
+++ docker/initdb/02-create-table-documents.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; 
+
+CREATE TABLE IF NOT EXISTS documents (
+  langchain_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  content      TEXT,
+  embedding    vector(1536),
+  metadata     JSONB
+);
